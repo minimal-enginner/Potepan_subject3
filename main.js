27f4ea04dabd5c0062e3ccf41aa3dc4eadb412e4
EC2 Default User <ec2-user@ip-172-31-6-180.ap-southeast-2.compute.internal>
2023-08-25 01:02:50 +0000

    ＜実践課題＞JavaScript(電卓)(小数点の不正入力を修正)

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let result = document.getElementById("result");
+let mode = 'integer_mode'; //最初は整数入力モード
 
 function edit(elem) {
   if (result.value == "0" || result.value == "00" ) {
@@ -13,17 +14,31 @@ function arithmetic(elem) {
       result.value.slice(-1) === "-" ||
       result.value.slice(-1) === "*" ||
       result.value.slice(-1) === "/" ||
-      result.value.slice(-1) === ".") {
+      result.value.slice(-1) === "." ) {
     return;
   }else{
+  mode = 'integer_mode'; //整数入力モード
+  result.value = result.value + elem.value;
+}
+}
+
+function period(elem) {
+  if (result.value.slice(-1) === ".") {
+    return;
+  }else if(mode == 'decimal_mode'){//小数点モード
+    return;
+  }else{
+  mode = 'decimal_mode'; //小数点モード
   result.value = result.value + elem.value;
 }
 }
   
 function calc() {
+  mode = 'integer_mode'; //整数入力モード
   result.value = new Function("return " + result.value)();
 }
 
 function update() {
+  mode = 'integer_mode'; //整数入力モード
   result.value = "";
 }
